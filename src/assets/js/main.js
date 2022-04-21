d9d18b7cb4ace4100e62057ce7d4ed9fd528f696
spirrit <olivemoonlighter@gmail.com>
2022-04-21 02:45:06 +0100

    feat: added sfx for menu navigation
    a preliminary step in the sound design

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -3,12 +3,16 @@
 //import rankingData from '../../players.json'
 const mrow = document.getElementById("feline");
 const meow = document.getElementById("cat_container");
-const cat = new Audio("assets/boing.ogg");
-function boing() {
-  cat.play();
+const cat = {
+  boingus: Object.assign(document.createElement('audio'), {
+    src: "assets/sound/boing.ogg",
+    volume: 1
+  })
 }
+
+
 function catMagic() {
-  boing();
+  cat.boingus.play();
   meow.classList.add("dansen");
   mrow.classList.add("acid");
 }
@@ -17,6 +21,13 @@ function youtube() {
     "https://www.youtube-nocookie.com/embed/w3VQUEmsO8M?autoplay=1";
 }
 function pageGrab() {
+  const boop = {
+    boopy: Object.assign(document.createElement('audio'), {
+      src: "assets/sound/boop.ogg",
+      volume: 0.75
+    })
+  }
+  boop.boopy.play();
   var page = document.getElementsByClassName("page");
   for (var i = 0; i < page.length; i++) {
     page[i].style.display = "none";
