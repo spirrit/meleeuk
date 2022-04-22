b582cf589b275809d618b9decf146a79928e6866
spirrit <olivemoonlighter@gmail.com>
2022-04-22 22:41:42 +0100

    fix: revamped and condensed navlink/audio code
    - combined separate navlink funcs into a single func
    - revamped the page turning function to accommodate the new funcs
    - moved the sound playing func into the page turner
    - js cleanup, nice
    
    big thanks to brik for teaching me a bunch of important concepts and helping to re-write everything
    
    Co-Authored-By: Adrien Boutigny <adrien@slash-tmp.dev>

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,8 +1,8 @@
 // add players json imports, perhaps in a seperate file
 //
 //import rankingData from '../../players.json'
-import boopSoundUrl from '../sound/boop.ogg'
-import boingSoundUrl from '../sound/boing.ogg'
+import boopSoundUrl from "../sound/boop.ogg";
+import boingSoundUrl from "../sound/boing.ogg";
 
 const mrow = document.getElementById("feline");
 const meow = document.getElementById("cat_container");
@@ -16,73 +16,52 @@ function youtube() {
   document.getElementById("ifr").src =
     "https://www.youtube-nocookie.com/embed/w3VQUEmsO8M?autoplay=1";
 }
-
 var audioCtx = new AudioContext();
 const panNode = new StereoPannerNode(audioCtx, {
   pan: 0,
 });
 const gainNode = new GainNode(audioCtx, {
-  gain: 0.4,
+  gain: 0.15,
 });
-let audioBuffer = null
+let audioBuffer = null;
 async function createBoopNode() {
   if (!audioBuffer) {
-    console.log('No cached audio buffer. Fetching the file...')
-
-    const response = await fetch(boopSoundUrl)
-    const arrayBuffer = await response.arrayBuffer()
-    const decodedBuffer = await audioCtx.decodeAudioData(arrayBuffer)
-    
-    audioBuffer = decodedBuffer
+    console.log("No cached audio buffer. Fetching the file...");
+    const response = await fetch(boopSoundUrl);
+    const arrayBuffer = await response.arrayBuffer();
+    const decodedBuffer = await audioCtx.decodeAudioData(arrayBuffer);
+    audioBuffer = decodedBuffer;
   }
-  
-  const bufferSourceNode = audioCtx.createBufferSource()
-  bufferSourceNode.buffer = audioBuffer
-
-  return bufferSourceNode
+  const bufferSourceNode = audioCtx.createBufferSource();
+  bufferSourceNode.buffer = audioBuffer;
+  return bufferSourceNode;
 }
-
-function pageGrab() {
-  createBoopNode().then(sourceNode => {
-    sourceNode.connect(panNode).connect(gainNode).connect(audioCtx.destination)
-    sourceNode.start(0)
-  })
-  var page = document.getElementsByClassName("page");
-  for (var i = 0; i < page.length; i++) {
-    page[i].style.display = "none";
+function changePage(pageName, panning) {
+  var pages = document.getElementsByClassName("page");
+  for (var i = 0; i < pages.length; i++) {
+    pages[i].style.display = "none";
   }
+  document.getElementById(pageName).style.display = "block";
+  createBoopNode().then((sourceNode) => {
+    sourceNode.connect(panNode).connect(gainNode).connect(audioCtx.destination);
+    panNode.pan.value = panning;
+    sourceNode.start(0);
+  });
 }
-function changeHome() {
-  panNode.pan.value = -0.5
-  pageGrab();
-  document.getElementById("home").style.display = "block";
+const navData = [
+  ["home", -0.5],
+  ["ranking", -0.25],
+  ["links", 0.25],
+  ["other", 0.5],
+];
+for (let i = 0; i < navData.length; i++) {
+  const navCurrentPage = navData[i];
+  document
+    .getElementById("nav" + navCurrentPage[0])
+    .addEventListener("click", function () {
+      changePage(navCurrentPage[0], navCurrentPage[1]);
+    });
 }
-function changeRanking() {
-  panNode.pan.value = -0.25
-  pageGrab();
-  document.getElementById("ranking").style.display = "block";
-}
-function changeLinks() {
-  panNode.pan.value = 0.25
-  pageGrab();
-  document.getElementById("links").style.display = "block";
-}
-function changeOther() {
-  panNode.pan.value = 0.5
-  pageGrab();
-  document.getElementById("other").style.display = "block";
-}
-function changeSecret() {
-  youtube();
-  pageGrab();
-  document.getElementById("secret").style.display = "block";
-}
-document.querySelectorAll(".navlink")[0].addEventListener("click", changeHome);
-document
-  .querySelectorAll(".navlink")[1]
-  .addEventListener("click", changeRanking);
-document.querySelectorAll(".navlink")[2].addEventListener("click", changeLinks);
-document.querySelectorAll(".navlink")[3].addEventListener("click", changeOther);
 document.getElementById("feline").addEventListener("click", catMagic);
 
 
