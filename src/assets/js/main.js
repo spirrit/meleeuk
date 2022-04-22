220f82bea6702dd08f991512c31eb6e64ceadd8e
spirrit <olivemoonlighter@gmail.com>
2022-04-22 19:43:38 +0100

    fix: changed nav sound loading to a more efficient method
    changed terrible xhr function to a more efficient sound loading system using fetch
    added panning node to the nav sound routing
    addded explicit height/width to images to prevent layout shifting on page load

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,18 +1,14 @@
 // add players json imports, perhaps in a seperate file
 //
 //import rankingData from '../../players.json'
+import boopSoundUrl from '../sound/boop.ogg'
+import boingSoundUrl from '../sound/boing.ogg'
+
 const mrow = document.getElementById("feline");
 const meow = document.getElementById("cat_container");
-const cat = {
-  boingus: Object.assign(document.createElement('audio'), {
-    src: "assets/sound/boing.ogg",
-    volume: 1
-  })
-}
-
-
+const cat = new Audio(boingSoundUrl);
 function catMagic() {
-  cat.boingus.play();
+  cat.play();
   meow.classList.add("dansen");
   mrow.classList.add("acid");
 }
@@ -20,32 +16,59 @@ function youtube() {
   document.getElementById("ifr").src =
     "https://www.youtube-nocookie.com/embed/w3VQUEmsO8M?autoplay=1";
 }
-function pageGrab() {
-  const boop = {
-    boopy: Object.assign(document.createElement('audio'), {
-      src: "assets/sound/boop.ogg",
-      volume: 0.75
-    })
+
+var audioCtx = new AudioContext();
+const panNode = new StereoPannerNode(audioCtx, {
+  pan: 0,
+});
+const gainNode = new GainNode(audioCtx, {
+  gain: 0.4,
+});
+let audioBuffer = null
+async function createBoopNode() {
+  if (!audioBuffer) {
+    console.log('No cached audio buffer. Fetching the file...')
+
+    const response = await fetch(boopSoundUrl)
+    const arrayBuffer = await response.arrayBuffer()
+    const decodedBuffer = await audioCtx.decodeAudioData(arrayBuffer)
+    
+    audioBuffer = decodedBuffer
   }
-  boop.boopy.play();
+  
+  const bufferSourceNode = audioCtx.createBufferSource()
+  bufferSourceNode.buffer = audioBuffer
+
+  return bufferSourceNode
+}
+
+function pageGrab() {
+  createBoopNode().then(sourceNode => {
+    sourceNode.connect(panNode).connect(gainNode).connect(audioCtx.destination)
+    sourceNode.start(0)
+  })
   var page = document.getElementsByClassName("page");
   for (var i = 0; i < page.length; i++) {
     page[i].style.display = "none";
   }
 }
 function changeHome() {
+  panNode.pan.value = -0.5
   pageGrab();
   document.getElementById("home").style.display = "block";
 }
-function changeLinks() {
-  pageGrab();
-  document.getElementById("links").style.display = "block";
-}
 function changeRanking() {
+  panNode.pan.value = -0.25
   pageGrab();
   document.getElementById("ranking").style.display = "block";
 }
+function changeLinks() {
+  panNode.pan.value = 0.25
+  pageGrab();
+  document.getElementById("links").style.display = "block";
+}
 function changeOther() {
+  panNode.pan.value = 0.5
   pageGrab();
   document.getElementById("other").style.display = "block";
 }
@@ -61,3 +84,6 @@ document
 document.querySelectorAll(".navlink")[2].addEventListener("click", changeLinks);
 document.querySelectorAll(".navlink")[3].addEventListener("click", changeOther);
 document.getElementById("feline").addEventListener("click", catMagic);
+
+
+createBoopNode();
