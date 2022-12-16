73f1880a48b9541c07c995d13e0d270dd21391f3
spirrit <olivemoonlighter@gmail.com>
2022-12-16 13:47:16 +0000

    refactored buggy js and commented

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,5 +1,7 @@
 import boopSoundUrl from "../sound/boop.ogg";
 
+// infernal sfx javascript that brik helped me write
+// needs a refactor/removal/documenting
 let audioCtx = new AudioContext();
 let gainNode = new GainNode(audioCtx, {
   gain: 0.15,
@@ -49,56 +51,61 @@ document.getElementById("burger").addEventListener("click", function(){
 
 const buttons = new Array(3);
 var jsonURL;
-function toggler() { //toggles display of sibling table
-  var tbSelecta = this.parentNode.querySelector(".tb");
-  var section = this.parentNode.id;
-  switch (section) { //determines which button was pressed, passes the right json
-    case "legendsection":
-      jsonURL = "";
-      jsonBourne(tbSelecta, jsonURL);
+function rankingButton() { 
+  // gets the data needed to show ranking table and populate with player data
+  var siblingTbl = this.parentNode.querySelector(".tb");
+  switch (this.parentNode.id) { 
+    //determines which button was pressed, passes the right json and array data
     case "pruk21":
       jsonURL = "/assets/json/pruk21.json";
-      jsonBourne(tbSelecta, jsonURL, 0);
+      jsonBourne(siblingTbl, jsonURL, 0);
       break;
     case "pruk22":
       jsonURL = "/assets/json/pruk22.json";
-      jsonBourne(tbSelecta, jsonURL, 1);
+      jsonBourne(siblingTbl, jsonURL, 1);
       break;
     case "prscot19":
       jsonURL = "/assets/json/prscot19.json";
-      jsonBourne(tbSelecta, jsonURL, 2);
+      jsonBourne(siblingTbl, jsonURL, 2);    
+    default:
+      console.log("nojson");
   }
-  function jsonBourne(table, jsonURL, button) { 
-    if (buttons[button] === undefined) { // if first press => populate table with json data
-      buttons[button] = true;
-      fetch(jsonURL)
-        .then(function (response) {
-            return response.json();
-        }).then(function (apiJsonData) {
-            console.log(apiJsonData);
-            renderJson(apiJsonData);
-        })
-      function renderJson(players) {
-        players.forEach(entry => {
-            let newRow = document.createElement("tr");
-            Object.values(entry).forEach((value) => {
-                let cell = document.createElement("td");
-                cell.innerHTML = value;
-                newRow.appendChild(cell);
-            })
-            table.appendChild(newRow);
-        });
-      };
-    };
-  }
-  console.log(this.parentNode.id); // toggle table display [they start as display:none]
-  if (tbSelecta.style.display === "table") {
-    tbSelecta.style.display = "none";
+  tableToggle(siblingTbl);
+}
+function tableToggle(x) { 
+  // toggle table display function
+  if (x.style.display === "table") {
+    x.style.display = "none";
   } else {
-    tbSelecta.style.display = "table";
+    x.style.display = "table";
   }
 }
-document.querySelectorAll(".tableheader").forEach(item => {
-  item.addEventListener('click', toggler)
-})
+function jsonBourne(table, json, y) { 
+  // ranking json handler
+  // if first press:
+  // - change array entry to disable loading json multiple times 
+  // - populate table with json data
+  if (buttons[y] === undefined) { 
+    buttons[y] = true;
+    fetch(json)
+      .then(function (response) {
+          return response.json();
+      }).then(function (apiJsonData) {
+          console.log(apiJsonData);
+          renderJson(apiJsonData);
+      })
+    function renderJson(players) {
+      players.forEach(entry => {
+          let newRow = document.createElement("tr");
+          Object.values(entry).forEach((value) => {
+              let cell = document.createElement("td");
+              cell.innerHTML = value;
+              newRow.appendChild(cell);
+          })
+          table.appendChild(newRow);
+      });
+    };
+  };
+}
+document.querySelectorAll(".tableheader").forEach(item => {item.addEventListener('click', rankingButton)});
 createBoopNode();
