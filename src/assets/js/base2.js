b90c7ac94c21ca613751aed9e996a5e56281600d
spirrit <olivemoonlighter@gmail.com>
2022-12-20 22:02:27 +0000

    implemented proper dynamic mobile support

--- src/assets/js/base2.js
+++ src/assets/js/base2.js
@@ -1,6 +1,9 @@
 document.getElementById("burger").addEventListener("click", function(){
   document.getElementById("wrapper").classList.toggle("link_wrapper_active");
 });
+document.getElementById("sidebar-burger").addEventListener("click", function(){
+  document.querySelector(".page-wrapper").classList.toggle("sidebar-active")
+});
 document.getElementById("cat").addEventListener("click", function(){
   document.getElementById("cat").classList.add("rainbow");
   document.getElementById("catp").appendChild(document.createTextNode("meow"));
--- src/assets/js/ranking.js
+++ src/assets/js/ranking.js
@@ -1,6 +1,9 @@
 document.getElementById("burger").addEventListener("click", function(){
   document.getElementById("wrapper").classList.toggle("link_wrapper_active");
 });
+document.getElementById("sidebar-burger").addEventListener("click", function(){
+  document.querySelector(".page-wrapper").classList.toggle("sidebar-active")
+});
 function tabler() { 
   let jsonURL;
   let table = document.querySelector(".tb");
