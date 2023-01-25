ba185e5a1c6feb6d393c0a258a432390c3a5195d
spirrit <olivemoonlighter@gmail.com>
2023-01-25 20:54:42 +0000

    edit cms config

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/sass/");
   eleventyConfig.addPassthroughCopy("./src/assets/");
   eleventyConfig.addPassthroughCopy("./src/_redirects");
-  eleventyConfig.addPassthroughCopy("./src/admin");
+  eleventyConfig.addPassthroughCopy("./src/static/admin");
 
   return {
     dir: {
